667115231a3a0be338f00e4453cc7dfcf387fcf4
VictorCiechovicz <victor.ciechovicz@merx.tech>
2023-06-22 15:13:15 -0300

    feact: create component Carcard and CarDetails

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,44 @@
-import { CustomFilter, SearchBar } from '@/components'
+import { CarCard, CustomFilter, SearchBar } from '@/components'
 import { Hero } from '@/components/Hero'
+import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars()
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
   return (
     <main className="overflow-hidden">
       <Hero />
 
-      <div className='mt-12 padding-x padding-y max-width' id="discover">
-        <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
-          <div className='home__filters'>
+          <div className="home__filters">
             <SearchBar />
-            <div className='home__filter-container'>
+            <div className="home__filter-container">
               <CustomFilter title="fuel" />
               <CustomFilter title="year" />
             </div>
-            
-</div>
+          </div>
+          {!isDataEmpty ? (
+            <section>
+              <div className="homme__cars-wrapper">
+                {allCars?.map(car => (
+                  <CarCard car={car} />
+                ))}
+              </div>
+            </section>
+          ) : (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">
+                Oops, sem resultado
+              </h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )}
         </div>
-
       </div>
     </main>
   )
