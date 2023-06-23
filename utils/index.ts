8cf98ec394d78242eb30320829873c7bc7a9d66c
VictorCiechovicz <victor.ciechovicz@merx.tech>
2023-06-23 16:50:14 -0300

    feat: create components customfilter, and showmore

--- utils/index.ts
+++ utils/index.ts
@@ -12,7 +12,7 @@ export async function fetchCars(filters: FilterProps) {
 
   }
 
-  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer ? manufacturer : 'acura'}&year=${year}model=${model}&limit=${limit}&fuel_type=${fuel}`, {
     headers: headers
   })
   const result = await response.json()
